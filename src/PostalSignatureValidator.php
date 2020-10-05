02c581f04818e81a2d2e8baee05eb2ee75bd0f9f
Robin Cramer <mail@robincramer.de>
2020-10-05 12:30:01 +0200

    Add signature validation.

--- src/PostalSignatureValidator.php
+++ src/PostalSignatureValidator.php
@@ -10,11 +10,18 @@ class PostalSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        // TODO: validate signature
-        return $request->input('signature.signature') === hash_hmac(
-            'sha256',
-            sprintf('%s%s', $request->input('signature.timestamp'), $request->input('signature.token')),
-            $config->signingSecret
-        );
+        $dkim_public_key = config('mailcoach.postal_feedback.public_key');
+
+        $rsa_key_pem = "-----BEGIN PUBLIC KEY-----\r\n" .
+            chunk_split($dkim_public_key, 64) .
+            "-----END PUBLIC KEY-----\r\n";
+        $rsa_key = openssl_pkey_get_public($rsa_key_pem);
+
+        $body = $request->getContent();
+
+        $signature = $request->header($config->signatureHeaderName);
+        $signature = base64_decode($signature);
+
+        return openssl_verify($body, $signature, $rsa_key, OPENSSL_ALGO_SHA1);
     }
 }
